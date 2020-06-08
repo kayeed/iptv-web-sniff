4d6773f7eb37b3a91755d70a3592c7f731e6346c
panda-mute <wxuzju@gmail.com>
2020-06-08 16:05:41 +0800

    update tvbnews

--- sniff/plugins/tvbnews_live.py
+++ sniff/plugins/tvbnews_live.py
@@ -5,6 +5,7 @@ import ipaddress
 import requests
 import random
 import json
+import time
 import re
 import os
 
@@ -15,15 +16,13 @@ class tvbnews_live(web_live):
 
         web_live.__init__(self, chname, request_info, extinfo, referer, logger)
 
-    def sniff_stream(self):
-
-        print("probe website %s ......"%(self.website))
+    def __web_player(self):
 
         ip_pool = [str(ip) for ip in ipaddress.IPv4Network('112.118.220.0/24')]
         ip = random.choice(ip_pool)
         token = "http://token.tvb.com/stream/live/hls/mobilehd_%s.smil?app=news&feed&client_ip=%s"%(self.chname, ip)
-
         #token = "http://token.tvb.com/stream/live/hls/mobilehd_%s.smil?app=news&feed&client_ip="%(self.chname)
+
         liveurl = "%s?token=%s"%(self.liveapi, urllib.parse.quote(token, safe=''))
         try:
             response = requests.get(liveurl, headers=self.headers)
@@ -36,24 +35,55 @@ class tvbnews_live(web_live):
         try:
             info = json.loads(response.text)
             link = info["url"]
+            return link
         except ValueError:
             self.logger.error(response.text)
             return None
 
+    def __app_player(self):
+
+        epoch = int(time.time()*1000)
+        liveurl = "http://token.tvb.com/stream/live/hls/mobilehd_%s.smil?app=news&feed&time=%d"%(self.chname, epoch)
+        self.headers['t'] = 'aea7cda047fc647bbde672a73c937f74'
         try:
-            response = requests.get(link, headers=self.headers, allow_redirects=False)
+            response = requests.get(liveurl, headers=self.headers)
             response.raise_for_status()
         except requests.exceptions.RequestException as err:
             self.logger.error(err)
             return None
+        response.encoding = 'utf-8'
 
-        if response.status_code == 302:
-            link = response.headers["Location"]
+        try:
+            info = json.loads(response.text)
+            link = info["url"]
+            return link
+        except ValueError:
+            self.logger.error(response.text)
+            return None
 
-        print("  {0: <20}{1:}".format(self.extinfo[4], link))
-        channel = self.extinfo + [link] + [self.headers["Referer"] if self.referer == 1 else ""]
-        self.link = link
-        return channel
+    def sniff_stream(self):
+
+        print("probe website %s ......"%(self.website))
+
+        link = self.__app_player()
+        if link:
+            try:
+                response = requests.get(link, headers=self.headers, allow_redirects=False)
+                response.raise_for_status()
+            except requests.exceptions.RequestException as err:
+                self.logger.error(err)
+                return None
+
+            if response.status_code == 302:
+                link = response.headers["Location"]
+
+            print("  {0: <20}{1:}".format(self.extinfo[4], link))
+            channel = self.extinfo + [link] + [self.headers["Referer"] if self.referer == 1 else ""]
+            self.link = link
+            return channel
+        else:
+            self.logger.error("m3u8 link not found!")
+            return None
 
     def sniff_m3u8_file(self, m3u8file):
 
