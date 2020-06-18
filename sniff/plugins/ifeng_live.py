6c166cce86e65669f4b9a1a03596de9babd41799
panda-mute <wxuzju@gmail.com>
2020-06-19 07:06:06 +0800

    update ifeng

--- sniff/plugins/ifeng_live.py
+++ sniff/plugins/ifeng_live.py
@@ -18,13 +18,37 @@ class ifeng_live(web_live):
 
         print("probe website %s ......"%(self.website))
 
-        epoch = "%x"%int(time.time()+1800)
-        input = "obb9Lxyv5C"+self.chname+epoch
-        link = self.liveapi%(self.chname, self.md5(input), epoch)
-        print("  {0: <20}{1:}".format(self.extinfo[4], link))
-        channel = self.extinfo + [link] + [self.headers["Referer"] if self.referer == 1 else ""]
-        self.link = link
-        return channel
+        liveurl = self.liveapi
+        try:
+            response = requests.get(liveurl, headers=self.headers)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as err:
+            self.logger.error(err)
+            return None
+        response.encoding = 'utf-8'
+
+        try:
+            info = json.loads(response.text)
+            for channel in info:
+                if channel["title"] == self.chname:
+                    liveurl = channel["live_url_fhd"]
+                    u = urlparse(liveurl)
+                    livepath = u.path.split(".")[0]
+
+                    epoch = "%x"%int(time.time()+1800)
+                    input = "obb9Lxyv5C"+livepath+epoch
+                    link = "%s?txSecret=%s&txTime=%s"%(liveurl, self.md5(input), epoch)
+                    print("  {0: <20}{1:}".format(self.extinfo[4], link))
+                    channel = self.extinfo + [link] + [self.headers["Referer"] if self.referer == 1 else ""]
+                    self.link = link
+                    return channel
+
+            self.logger.error(info)
+            return None
+        except (ValueError, KeyError) as err:
+            self.logger.error("%s - %s"%(err,response.text))
+            return None
+
 
     def sniff_m3u8_file(self, m3u8file):
 
