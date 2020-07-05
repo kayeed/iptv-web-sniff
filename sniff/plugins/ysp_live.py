4fb5a670a15ed058674837e730d2057f329385d7
panda-mute <wxuzju@gmail.com>
2020-07-05 14:30:06 +0800

    add ysp plugin

---
+++ sniff/plugins/ysp_live.py
@@ -0,0 +1,33 @@
+from sniff.web_live import web_live, is_url
+
+import subprocess
+import requests
+import hashlib
+import random
+import m3u8
+import json
+import time
+import re
+import os
+
+
+class ysp_live(web_live):
+
+    def __init__(self, chname, request_info, extinfo, referer, logger):
+
+        web_live.__init__(self, chname, request_info, extinfo, referer, logger)
+
+    def sniff_stream(self):
+
+        print("probe website %s ......"%(self.website))
+        liveurl = self.liveapi
+
+        link = self.http_redirect(liveurl)
+        print("  {0: <20}{1:}".format(self.extinfo[4], link))
+        channel = self.extinfo + [link] + [self.headers["Referer"] if self.referer == 1 else ""]
+        self.link = link
+        return channel
+
+    def sniff_m3u8_file(self, m3u8file):
+
+        self.dump_custom_m3u8(self.link, m3u8file)
